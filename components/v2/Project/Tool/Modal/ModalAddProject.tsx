4c05e3e28dbd60bf7b9e4756bdabe152171ea1eb
VOY1511 <anhvi2898@gmail.com>
2025-10-22 16:22:19 +0700

    update

--- components/v2/Project/Tool/Modal/ModalAddProject.tsx
+++ components/v2/Project/Tool/Modal/ModalAddProject.tsx
@@ -103,9 +103,12 @@ const addUserRole = () => {
   }
   const handleSubmit = async (values: ICreateProject) => {
     try {
+      
       const formdata = CustomFormData(
         Object.entries({
           ...values,
+          start_date:values.start_date ? values.start_date.toISOString():undefined,
+          end_date:values.end_date ? values.end_date.toISOString():undefined,
           picture_url: [filePicture?.originFileObj as File],users:JSON.stringify(userRoles)
         }).reduce((acc: any, [key, value]) => {
           if (value !== null && value !== undefined) {
